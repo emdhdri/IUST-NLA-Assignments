e11111c2d678f0d577f0fa19661dbc307ed31869
Emad Heidari <emdhdri@gmail.com>
2024-11-10 21:54:20 +0330

    add LU system solver functions

--- LU_decomposition.py
+++ LU_decomposition.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 
+
 def doolittle_decomposition(matrix):
     shape = np.shape(matrix)
     if len(shape) != 2 or shape[0] != shape[1]:
@@ -45,3 +46,38 @@ def crout_decomposition(matrix):
             U[i][j] = (matrix[i][j] - sum) / L[i][i]
 
     return L, U
+
+
+def solve_lower_triangular_system(L, b):
+    x = np.zeros(len(b))
+
+    for i in range(len(b)):
+        sum = 0
+        for j in range(i):
+            sum += x[j] * L[i][j]
+        x[i] = (b[i] - sum) / L[i][i]
+
+    return x
+
+
+def solve_upper_triangular_system(U, b):
+    n = len(b)
+    x = np.zeros(n)
+
+    for i in reversed(range(n)):
+        sum = 0
+        for j in range(i + 1, n):
+            sum += x[j] * U[i][j]
+        x[i] = (b[i] - sum) / U[i][i]
+        
+    return x
+
+
+def LU_decomposition_system_solver(matrix, b, decomposition_function):
+    L, U = decomposition_function(matrix)
+
+    z = solve_lower_triangular_system(L, b)
+    x = solve_upper_triangular_system(U, z)
+    
+    return x
+
